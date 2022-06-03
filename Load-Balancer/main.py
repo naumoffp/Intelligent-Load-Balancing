a79ab4d28b0a2a70ea57dee2697bc5d05a3e762d
Peter <places.sachems-0c@icloud.com>
2022-06-02 18:28:18 -0700

    Create dev workflow

---
+++ Load-Balancer/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("This is the load balancer")
+
+if __name__ == "__main__":
+    main()
---
+++ Server-Pool/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("This is the server pool")
+
+if __name__ == "__main__":
+    main()
